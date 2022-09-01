3ae661eab08cdaaa25b245414a7fe3f27aa1f952
Echenique Lucas <martinechenique94@gmail.com>
2022-08-31 22:20:30 -0300

    Hasta metodo btnDetalle_Click()

--- src/BugTracker/frmConsultaBugs.cs
+++ src/BugTracker/frmConsultaBugs.cs
@@ -15,6 +15,7 @@ namespace BugTracker
         public frmConsultaBugs()
         {
             InitializeComponent();
+            dgvBugs.AutoGenerateColumns = false;
         }
 
       
@@ -107,5 +108,19 @@ namespace BugTracker
 
         }
 
+        private void dgvBugs_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnDetalle.Enabled = true;
+        }
+
+        private void btnDetalle_Click(object sender, EventArgs e)
+        {
+            if(dgvBugs.CurrentRow != null) { 
+            frmDetalleBug frmDetalle = new frmDetalleBug();
+         //   int idBug = dgvBugs.CurrentRow.DataBoundItem()
+            frmDetalle.InicializarDetalleBug(1);
+            frmDetalle.ShowDialog();
+            }
+        }
     }
 }
