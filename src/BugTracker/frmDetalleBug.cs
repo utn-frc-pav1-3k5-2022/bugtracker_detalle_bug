6af9b915f0e50d667595c154c03296eb7a62ee74
Christian Emmanuel Pirra <christianpirra@gmail.com>
2022-10-26 20:57:10 -0300

    Modificaciones para la entrega

--- src/BugTracker/frmDetalleBug.cs
+++ src/BugTracker/frmDetalleBug.cs
@@ -25,19 +25,58 @@ namespace BugTracker
 
         internal void InicializarDetalleBug(int idBug)
         {
-            DataTable dt;
-            string query = "SELECT * FROM BUGS WHERE id_bug = " + idBug;
+            //DataTable dt;
+            //string query = "SELECT * FROM BUGS WHERE id_bug = " + idBug;
 
-            dt = DataManager.GetInstance().ConsultaSQL(query);
+            //dt = DataManager.GetInstance().ConsultaSQL(query);
 
-            this.txtCriticidad.Text = dt.Rows[0]["id_criticidad"].ToString();
-            this.txtDescripcion.Text = dt.Rows[0]["descripcion"].ToString();
-            this.txtEstado.Text = dt.Rows[0]["id_estado"].ToString();
-            this.txtFechaAlta.Text = dt.Rows[0]["fecha_alta"].ToString();
-            this.txtNroBug.Text = dt.Rows[0]["id_bug"].ToString();
-            this.txtPrioridad.Text = dt.Rows[0]["id_prioridad"].ToString();
-            this.txtProducto.Text = dt.Rows[0]["id_producto"].ToString();
-            this.txtTitulo.Text = dt.Rows[0]["titulo"].ToString();
+            //this.txtCriticidad.Text = dt.Rows[0]["id_criticidad"].ToString();
+            //this.txtDescripcion.Text = dt.Rows[0]["descripcion"].ToString();
+            //this.txtEstado.Text = dt.Rows[0]["id_estado"].ToString();
+            //this.txtFechaAlta.Text = dt.Rows[0]["fecha_alta"].ToString();
+            //this.txtNroBug.Text = dt.Rows[0]["id_bug"].ToString();
+            //this.txtPrioridad.Text = dt.Rows[0]["id_prioridad"].ToString();
+            //this.txtProducto.Text = dt.Rows[0]["id_producto"].ToString();
+            //this.txtTitulo.Text = dt.Rows[0]["titulo"].ToString();
+
+            string sql = "SELECT bug.id_bug, " +
+                          "bug.titulo," +
+                          "bug.descripcion," +
+                          "bug.fecha_alta," +
+                          "bug.id_usuario_responsable," +
+                          "responsable.usuario as responsable,  " +
+                          "bug.id_usuario_asignado," +
+                          "asignado.usuario as asignado," +
+                          "bug.id_producto," +
+                          "producto.nombre as producto, " +
+                          "bug.id_prioridad," +
+                          "prioridad.nombre as prioridad," +
+                          "bug.id_criticidad," +
+                          "criticidad.nombre as criticidad, " +
+                          "bug.id_estado," +
+                          "estado.nombre as estado" +
+                      "  FROM Bugs as bug" +
+                      "  LEFT JOIN Usuarios as responsable ON responsable.id_usuario = bug.id_usuario_responsable" +
+                      "  LEFT JOIN Usuarios as asignado ON asignado.id_usuario = bug.id_usuario_asignado" +
+                      " INNER JOIN Productos as producto ON producto.id_producto = bug.id_producto" +
+                      " INNER JOIN Prioridades as prioridad ON prioridad.id_prioridad = bug.id_prioridad" +
+                      " INNER JOIN Criticidades as criticidad ON criticidad.id_criticidad = bug.id_criticidad" +
+                      " INNER JOIN Estados as estado ON estado.id_estado = bug.id_estado" +
+                        " WHERE id_bug = '" + idBug + "'";
+
+            DataTable dtBug = DataManager.GetInstance().ConsultaSQL(sql);
+            //txtTitulo.Text = dtBug.Rows[0]["titulo"].ToString();
+            //txtProducto.Text = dtBug.Rows[0]["producto"].ToString();
+
+            this.txtCriticidad.Text = dtBug.Rows[0]["criticidad"].ToString();
+            this.txtDescripcion.Text = dtBug.Rows[0]["descripcion"].ToString();
+            this.txtEstado.Text = dtBug.Rows[0]["estado"].ToString();
+            this.txtFechaAlta.Text = dtBug.Rows[0]["fecha_alta"].ToString();
+            this.txtNroBug.Text = dtBug.Rows[0]["id_bug"].ToString();
+            this.txtPrioridad.Text = dtBug.Rows[0]["prioridad"].ToString();
+            this.txtProducto.Text = dtBug.Rows[0]["producto"].ToString();
+            this.txtTitulo.Text = dtBug.Rows[0]["titulo"].ToString();
+
         }
     }
 }
